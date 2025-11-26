2e4ccdeeb55036bc95acc26b9e024bd3b6fd39ed
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2025-11-26 14:37:25 +0000

    [Update]top view

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,10 @@ Rails.application.routes.draw do
   get 'customers/withdraw'
   get 'items/index'
   get 'items/show'
-  get 'homes/top'
   get 'homes/about'
+
+  root to: "homes#top"
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
